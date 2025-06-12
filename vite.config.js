ca31923b596f113bcedfead5b24b3c97073074ca
KarthickACL <karthick_k@ambattur.com>
2025-06-12 21:36:44 +0530

    first-commit

---
+++ vite.config.js
@@ -0,0 +1,82 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png'
+      ],
+      manifest: {
+        name: 'm-notification',
+        short_name: 'ReactPWA',
+        description: 'Notification system',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+         {
+        src: "./android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "./android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        // purpose:'favicon'
+         purpose: 'any maskable'
+      },
+      // {
+      //   src: '/apple-touch-icon.png',
+      //   sizes:'180x180',
+      //   type:'image/png',
+      //   purpose:'apple touch icon',
+      // },
+      // {
+      //   src: "./icon-144x144.png",
+      //   sizes: "144x144",
+      //   type: "image/png",
+      //   purpose: "any"
+      // },
+      // {
+      //   src: "./icon-256x256.png",
+      //   sizes: "256x256",
+      //   type: "image/png",
+      //   purpose: "icon"
+      // },
+      // {
+      //   src: "./icon-384x384.png",
+      //   sizes: "384x384",
+      //   type: "image/png",
+      //   purpose: "any maskable"
+      // }
+        ]
+      }
+      , workbox: {
+    globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+    runtimeCaching: [
+      {
+        urlPattern: ({ request }) => request.destination === 'document',
+        handler: 'NetworkFirst'
+      }
+    ]
+  }
+    })
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 4173,
+    strictPort: true
+  }
+});
+
